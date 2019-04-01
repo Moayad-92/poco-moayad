e81ed6b1e9a33eb13d2ac7e8de59c0b1dcaefb3f
moayad92 <moayad.alobaid@powercoders.org>
2019-04-01 16:55:38 +0200

    Updating JS file

--- change_para_class/setClass.js
+++ change_para_class/setClass.js
@@ -1,17 +1,4 @@
-console.log('i am here');
-
 let el = document.getElementById('mypara');
-
-
-function setClassWarning() {
-    el.className = 'warning';
-}
-
-function setClassTip() {
-
-    el.className = 'tip';
-}
-
-function clearClass() {
-    el.className = '';
+function setClass(classN) {
+    el.className = classN;
 }
