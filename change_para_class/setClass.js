42c0ae8be303dd763dc7edb093da66fdbf4acc85
moayad92 <moayad.alobaid@powercoders.org>
2019-04-01 16:19:21 +0200

    Updating index.html and creating JS file

---
+++ change_para_class/setClass.js
@@ -0,0 +1,17 @@
+console.log('i am here');
+
+let el = document.getElementById('mypara');
+
+
+function setClassWarning() {
+    el.className = 'warning';
+}
+
+function setClassTip() {
+
+    el.className = 'tip';
+}
+
+function clearClass() {
+    el.className = '';
+}
